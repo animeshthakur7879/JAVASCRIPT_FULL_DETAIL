d5bbdff5fb2a920e9eb7137a68a4f2cc2eb39f5e
animeshthakur7879 <animeshthakur7879@gmail.com>
2025-12-10 21:27:47 +0530

    Async started

---
+++ 07_Async/01_Async.js
@@ -0,0 +1,17 @@
+/*
+    ASYNC JS 
+
+    We know that the default JS is Synchronous
+                                   Single Threaded 
+
+    But this is all default , now a days it is more advance that can handle async operations 
+
+
+    #Blocking code v/s Non Blocking code 
+            Blocking Code => Block sthe flow of entire program 
+                             
+
+            Non Blocking  => Doesnt block the excecution of code
+
+
+*/
